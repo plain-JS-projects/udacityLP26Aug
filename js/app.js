6c7defbea40a8cf13a9680b627de16a03b678dd1
bassant <bassant.ayman69@gmail.com>
2020-09-10 23:28:53 +0300

    add smooth scrolling

--- js/app.js
+++ js/app.js
@@ -17,19 +17,17 @@
  * Define Global Variables
  *
  */
-const section = document.querySelectorAll("section"),
-  navBarList = document.getElementById("navbar__list"),
-  sectionsData = Array.from(document.getElementsByTagName("section")),
-  menuItems = sectionsData.map((el) => ({
+const navBarList = document.getElementById("navbar__list"),
+  sections = Array.from(document.getElementsByTagName("section")),
+  menuItems = sections.map((el) => ({
     link: `#${el.id}`,
     label: el.dataset.nav,
   }));
 
-let sections = {},
-  i = 0;
+let sectionsOffsetTop = {};
 
-Array.from(section).forEach((e) => {
-  sections[e.id] = e.offsetTop;
+sections.forEach((e) => {
+  sectionsOffsetTop[e.id] = e.offsetTop;
 });
 /**
  * End Global Variables
@@ -44,21 +42,26 @@ Array.from(section).forEach((e) => {
  */
 
 // build the nav
-menuItems.forEach((element, i) => {
-  const li = document.createElement("li");
-  const a = document.createElement("a");
+menuItems.forEach((element, index) => {
+  const li = document.createElement("li"),
+    a = document.createElement("a");
+
   a.href = element.link;
   a.innerText = element.label;
-  if (i === 0) {
+
+  if (index === 0) {
     a.classList.add("active");
   }
-  a.addEventListener("click", () => {
+
+  a.addEventListener("click", (e) => {
+    let targetBlock = document.querySelector(e.target.hash);
+    e.preventDefault();
     window.scrollTo({
-      left: 0,
-      top: sections[element.link],
+      top: targetBlock.offsetTop,
       behavior: "smooth",
     });
   });
+
   li.appendChild(a);
   navBarList.appendChild(li);
 });
@@ -82,16 +85,16 @@ window.onscroll = function () {
   var scrollPosition =
     document.documentElement.scrollTop || document.body.scrollTop;
 
-  for (i in sections) {
-    if (sections[i] <= scrollPosition) {
+  for (el in sectionsOffsetTop) {
+    if (sectionsOffsetTop[el] <= scrollPosition) {
       document.querySelector(".active").setAttribute("class", "");
       document
-        .querySelector("a[href*=" + i + "]")
+        .querySelector("a[href*=" + el + "]")
         .setAttribute("class", "active");
 
       //   document
       document.querySelector("section.active").setAttribute("class", " ");
-      document.getElementById(i).setAttribute("class", "active");
+      document.getElementById(el).setAttribute("class", "active");
     }
   }
 };
